2c0456a5d06f7bd9790eb2a76b966caa70a80a1d
Harshil Prajapati <harshil9913435191@gmail.com>
2026-09-08 11:52:45 +0530

    solve problem

--- src/pages/Master/BankMaster/useBankMasterConfig.jsx
+++ src/pages/Master/BankMaster/useBankMasterConfig.jsx
@@ -1,66 +1,63 @@
-import React from "react";
-
 const useCityMasterConfig = ({ handleDelete } = {}) => {
-    const bankListingColDef = [
-        {
-            id: "bankName",
-            field: "bankName",
-            headerName: "Bank Name",
-            width: 120,
-        },
-        {
-            id: "shortName",
-            field: "shortName",
-            headerName: "Short Name",
-            width: 100,
-        },
-        {
-            id: "action",
-            field: "action",
-            headerName: "Action",
-            width: 80,
-            type: "actions",
-            onClick: (data) => {
-                handleDelete(data._id);
-            }
-        }
-    ];
+  const bankListingColDef = [
+    {
+      id: "bankName",
+      field: "bankName",
+      headerName: "Bank Name",
+      width: 200,
+    },
+    {
+      id: "shortName",
+      field: "shortName",
+      headerName: "Short Name",
+      width: 100,
+    },
+    {
+      id: "action",
+      field: "action",
+      headerName: "Action",
+      width: 80,
+      type: "actions",
+      onClick: (data) => {
+        handleDelete(data._id);
+      },
+    },
+  ];
 
-    const initialValue = {
-        bankName: "",
-        shortName: "",
-    };
+  const initialValue = {
+    bankName: "",
+    shortName: "",
+  };
 
+  const bankSchema = [
+    {
+      id: "bankName",
+      type: "text",
+      label: "Bank Name",
+      placeHolder: "Enter Bank Name",
+      required: true,
+      prevFocusField: "bankName",
+      nextFocusField: "shortName",
+      className: "col-span-12",
+      textTransform: "uppercase",
+    },
+    {
+      id: "shortName",
+      type: "text",
+      label: "Short Name",
+      placeHolder: "Enter Short Name",
+      required: true,
+      prevFocusField: "bankName",
+      nextFocusField: "shortName",
+      className: "col-span-12",
+    },
+  ];
 
-    const bankSchema = [
-        {
-            id: "bankName",
-            type: "text",
-            label: "Bank Name",
-            placeHolder: "Enter Bank Name",
-            required: true,
-            prevFocusField: "bankName",
-            nextFocusField: "shortName",
-            className: "col-span-12",
-            textTransform: "uppercase",
-        },
-        {
-            id: "shortName",
-            type: "text",
-            label: "Short Name",
-            placeHolder: "Enter Short Name",
-            required: true,
-            prevFocusField: "bankName",
-            nextFocusField: "shortName",
-            className: "col-span-12",
-        },
-    ];
-
-    return {
-        bankListingColDef,
-        bankSchema,
-        initialValue
-    };
+  return {
+    bankListingColDef,
+    bankSchema,
+    initialValue,
+  };
 };
 
 export default useCityMasterConfig;
